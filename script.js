725609a576c5b4c569469b95119dc7ccb0da8862
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-08 00:08:11 +0200

    button container

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleAccordion() {
+    const content = document.getElementById('accordionContent');
+    content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+  }
